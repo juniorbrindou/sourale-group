f5dc1eed55741e7c8fdae679c9140091e680bf1c
225junior <225juniorbrindou@gmail.com>
2021-06-08 13:53:28 +0000

    add lang fr

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+	'failed' => "Ces informations d'identification ne correspondent pas à nos enregistrements.",
+	'throttle' => 'Trop de tentatives. Veuillez réessayer dans :seconds secondes.',
+
+];
