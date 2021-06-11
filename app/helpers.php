3d03f80258239c5dcc7a20f4a3bc1987e46e4ddd
225junior <225juniorbrindou@gmail.com>
2021-06-11 11:19:03 +0000

    Amélioration du dashboard et debut de breadcrumbs

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,23 @@
 <?php
+
+/**
+ * Fonction utili
+ **/
 if(! function_exists('test')){
-    function test(){
-        echo 'salut tout le monde';
-    }
+	function test(){
+		echo 'salut tout le monde';
+	}
+}
+
+if (! function_exists('page_title')) {
+	function page_title(){
+
+		if (request()->is('dashboard')) {
+			echo 'Tableau de bord';
+		}else if (request()->is('dashboard')){
+			echo 'string';
+		}else{
+			echo '';
+		}
+	}
 }
