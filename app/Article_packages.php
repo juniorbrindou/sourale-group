614dfd85f80b7dda66638931a77dd28dbaf980f6
225junior <225juniorbrindou@gmail.com>
2021-07-31 13:39:44 +0000

    correction bd

--- app/Article_packages.php
+++ app/Article_packages.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article_packages extends Model
 {
-    //
+    /**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+
+		'qte', 'article_id', 'package_id'
+	];
 }
