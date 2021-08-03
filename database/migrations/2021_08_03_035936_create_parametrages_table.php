e77159225c031097e0847a38e8ddec9f4ce9d46c
225junior <225juniorbrindou@gmail.com>
2021-08-03 05:39:15 +0000

    Add parametrage table

---
+++ database/migrations/2021_08_03_035936_create_parametrages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParametragesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parametrages', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->string('libelle')->nullable();
+            $table->string('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parametrages');
+    }
+}
