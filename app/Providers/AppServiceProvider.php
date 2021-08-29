89c323a34e200c70c7269980f49c9294a7e9087f
225junior <225juniorbrindou@gmail.com>
2021-08-29 15:13:45 +0000

    ajout de helper de carbonne

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(200);
+        Carbon::setLocale(config('app.locale', 'fr'));
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,71 +1,103 @@
 <?php
 
+use Carbon\Carbon;
+
 /**
  * Fonction utili
  **/
 
+if (!function_exists('long_date')) {
+    /**
+     * retourne la date au format humain de carbone
+     * ex: long_date(date('Y-m-d H:i'))
+     * ex: dimanche 29 août 2021 14:01
+     * si aucun parametre, retourne la date du jour
+     *
+     * @param string|null $date
+     * @return string| date
+     */
+    function long_date(string $date = null)
+    {
+        if ($date == null) {
+            return Carbon::now()->isoFormat('LLLL');
+        } else {
+            $date = Carbon::parse($date);
+            return $date->isoFormat('LLLL');
+        }
+    }
+}
+
+
+
+if (!function_exists('format_money')) {
+    function format_money(float $montant)
+    {
+        return number_format($montant, 0, ',', ' ');
+    }
+}
+
 if (!function_exists('format_no_array')) {
 
-	function format_no_array($array)
-	{
-		$skips = ["[", "]", "\""];
-		return str_replace($skips, ' ', $array);
-	}
+    function format_no_array($array)
+    {
+        $skips = ["[", "]", "\""];
+        return str_replace($skips, ' ', $array);
+    }
 }
 
 if (!function_exists('page_title')) {
-	function page_title()
-	{
+    function page_title()
+    {
 
-		$title = last(request()->segments());
-		return $title;
-	}
+        $title = last(request()->segments());
+        return $title;
+    }
 }
 
 if (!function_exists('getTile')) {
-	/**
-	 * Retourne un titre dynamique pour le titre des pages
-	 * @return Title @string 
-	 */
-	function getTile()
-	{
+    /**
+     * Retourne un titre dynamique pour le titre des pages
+     * @return Title @string
+     */
+    function getTile()
+    {
 
-		if (request()->is('dashboard')) {
-			return 'Tableau de bord';
-		} else if (request()->is('categorieArticles*')) {
-			return 'Catégories d\'articles';
-		} else if (request()->is('clients*')) {
-			return 'Clients';
-		} else if (request()->is('fournisseurs*')) {
-			return 'Fournisseurs';
-		} else if (request()->is('typeArticles*')) {
-			return 'Type des articles';
-		} else if (request()->is('typeEvenements*')) {
-			return 'Type d\'événements';
-		} else if (request()->is('users*')) {
-			return 'Utilisateurs';
-		} else {
-			echo '';
-		}
-	}
+        if (request()->is('dashboard')) {
+            return 'Tableau de bord';
+        } else if (request()->is('categorieArticles*')) {
+            return 'Catégories d\'articles';
+        } else if (request()->is('clients*')) {
+            return 'Clients';
+        } else if (request()->is('fournisseurs*')) {
+            return 'Fournisseurs';
+        } else if (request()->is('typeArticles*')) {
+            return 'Type des articles';
+        } else if (request()->is('typeEvenements*')) {
+            return 'Type d\'événements';
+        } else if (request()->is('users*')) {
+            return 'Utilisateurs';
+        } else {
+            echo '';
+        }
+    }
 }
 
 if (!function_exists('userAvatar')) {
-	/**
-	 * Recoit le genre de l'utilisateur, retourne un avatar en fonction du genre
-	 * @return avatar @string 
-	 */
-	function userAvatar(string $GenreUser)
-	{
-		if ($GenreUser === 'Mme') {
-			$avatar = asset('dist/img/avatar3.png');
-		} elseif ($GenreUser === 'M') {
-			$avatar = asset('dist/img/avatar.png');
-		} elseif ($GenreUser === 'Mlle') {
-			$avatar = asset('dist/img/34.jpg');
-		} else {
-			$avatar = asset('dist/img/25.jpg');
-		}
-		return $avatar;
-	}
+    /**
+     * Recoit le genre de l'utilisateur, retourne un avatar en fonction du genre
+     * @return avatar @string
+     */
+    function userAvatar(string $GenreUser)
+    {
+        if ($GenreUser === 'Mme') {
+            $avatar = asset('dist/img/avatar3.png');
+        } elseif ($GenreUser === 'M') {
+            $avatar = asset('dist/img/avatar.png');
+        } elseif ($GenreUser === 'Mlle') {
+            $avatar = asset('dist/img/34.jpg');
+        } else {
+            $avatar = asset('dist/img/25.jpg');
+        }
+        return $avatar;
+    }
 }
