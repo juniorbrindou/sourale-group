50bbb7bc378e1a6c116a4099c12f40acf917957e
225junior <35270982+225junior@users.noreply.github.com>
2022-01-23 04:30:24 +0000

    mise en place de la branche de developpement

--- config/database.php
+++ config/database.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Str;
 
-# Pour heroku
-$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
-$DB_CONNECTION = parse_url(getenv("DB_CONNECTION"));
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -69,12 +65,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL["host"],
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => $DATABASE_URL["port"],
-            // 'port' => env('DB_PORT', '5432'),
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            // 'database' => env('DB_DATABASE', 'forge'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
             'dump' => [
                 'excludeTables' => [
                     'table_to_exclude_from_backup',
@@ -82,15 +75,12 @@ return [
                 ]
             ],
 
-            'username' => $DATABASE_URL["user"],
-            // 'username' => env('DB_USERNAME', 'forge'),
-            'password' => $DATABASE_URL["pass"],
-            // 'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
